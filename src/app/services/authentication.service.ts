aaec2ab4d1a0cba6224f5522a792d90526c2fe49
Sarthak Bhatnagar <yankees1fan2002@gmail.com>
2019-07-16 09:32:58 -0400

    Created a hard coded an array to store user login information

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
+
+  users = ['user1', 'user2', 'user3'];
+  passwords = ['password', 'password', 'password'];
 
   authenticate(username, password) {
-    if (username === "user" && password === "password") {
-      sessionStorage.setItem('username', username);
-      return true;
-    } else {
+    const userIndex = this.users.indexOf(username);
+    if (userIndex == -1) {
       return false;
+    } else {
+      if (password == this.passwords[userIndex]) {
+        sessionStorage.setItem('username', username);
+        return true;
+      }
     }
+    // if (username === "user" && password === "password") {
+    //   sessionStorage.setItem('username', username);
+    //   return true;
+    // } else {
+    //   return false;
+    // }
   }
 
   isUserLoggedIn() {
