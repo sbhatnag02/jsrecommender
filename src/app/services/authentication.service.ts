7cbd83f96a375bb0363800e00bb711db23d1466f
Sarthak Bhatnagar <yankees1fan2002@gmail.com>
2019-07-16 10:46:44 -0400

    Revert "Created a hard coded an array to store user login information"
    This reverts commit aaec2ab4d1a0cba6224f5522a792d90526c2fe49.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,31 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private http: HttpClientModule) { }
-
-  users = ['user1', 'user2', 'user3'];
-  passwords = ['password', 'password', 'password'];
+  constructor() { }
 
   authenticate(username, password) {
-    const userIndex = this.users.indexOf(username);
-    if (userIndex == -1) {
-      return false;
+    if (username === "user" && password === "password") {
+      sessionStorage.setItem('username', username);
+      return true;
     } else {
-      if (password == this.passwords[userIndex]) {
-        sessionStorage.setItem('username', username);
-        return true;
-      }
+      return false;
     }
-    // if (username === "user" && password === "password") {
-    //   sessionStorage.setItem('username', username);
-    //   return true;
-    // } else {
-    //   return false;
-    // }
   }
 
   isUserLoggedIn() {
