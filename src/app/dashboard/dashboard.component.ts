7e4a8a22f2102d9e940da6f47eafe5e494d11bca
Sarthak Bhatnagar <sarthak.bhatnagar@jhuapl.edu>
2019-07-23 15:03:36 -0400

    Added Radar, Polar Area, and Doughnut Charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -23,34 +23,12 @@ export class DashboardComponent implements OnInit {
   LineChart = [];
   BarChart = [];
   PieChart = [];
-  chartColors = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(149, 255, 0, 0.2)',
-    'rgba(255, 229, 0, 0.2)',
-    'rgba(0, 149, 255, 0.2)',
-    'rgba(255, 247, 0, 0.2)',
-    'rgba(255, 72, 0, 0.2)',
-    'rgba(127, 0, 255, 0.2)'
-  ];
-  chartBorders = [
-    'rgba(255, 99, 132, 1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-    'rgba(149, 255, 0, 1)',
-    'rgba(255, 229, 0, 1)',
-    'rgba(0, 149, 255, 1)',
-    'rgba(255, 247, 0, 1)',
-    'rgba(255, 72, 0, 1)',
-    'rgba(127, 0, 255, 1)'
-  ];
+  RadarChart = [];
+  PolarAreaChart = [];
+  DoughnutChart = [];
+
+  chartColors = [];
+  chartBorders = [];
 
   labels = [];
   chartData = [];
@@ -68,6 +46,75 @@ export class DashboardComponent implements OnInit {
   goBackToFileUpload() {
     this.dataService.removeData(this.dataKey);
     this.router.navigate(['file-upload']);
+  }
+
+  getDataList() {
+    this.dataLength = 0;
+    this.dataService.getDataFromFirebase().snapshotChanges().forEach(dataSnapshots => {
+      this.dataList = [];
+      dataSnapshots.forEach(dataSnapshot => {
+        let dataItem = dataSnapshot.payload.val();
+        this.dataAsJSON = dataSnapshot.payload.toJSON();
+        console.log(this.dataAsJSON);
+
+        this.dataLength = Object.keys(this.dataAsJSON).length;
+        console.log(this.dataLength);
+
+        console.log(this.dataAsJSON[7].Month);
+        console.log(this.dataAsJSON[7].Sold);
+        console.log(this.dataAsJSON[15] == null);
+
+        this.updateLocalData();
+
+        dataItem['$key'] = dataSnapshot.key;
+        this.dataKey = dataSnapshot.key;
+        this.dataList.push(dataItem as IData);
+      });
+    });
+  }
+
+  updateLocalData() {
+    let index = 0;
+    while(index < this.dataLength){
+      if(this.dataAsJSON[index] != null) {
+        this.labels.push(this.dataAsJSON[index].Month);
+        this.chartData.push(this.dataAsJSON[index].Sold);
+      }
+      console.log(index);
+      index++;
+    }
+    console.log(this.labels[0]);
+    this.generateColors(this.dataLength);
+    this.generateGraphs();
+  }
+
+  generateGraphs() {
+    this.generateLineChart();
+    this.generateBarChart();
+    this.generatePieChart();
+    this.generateRadarGraph();
+    this.generatePolarAreaChart();
+    this.generateDoughnutChart();
+  }
+
+  generateColors(numColors: number) {
+    let index = 0;
+    while(index < numColors){
+      let newColor = 'rgba(' + this.randomInt(50, 200) + ', ' + this.randomInt(50, 200) + ', ' +
+      this.randomInt(50, 200) + ', ' + this.randomInt(0.5, 1) + ')';
+      this.chartColors[index] = newColor;
+      this.chartBorders[index] = newColor;
+      index = index + 1;
+    }
+  }
+
+  generateColor() {
+    return 'rgba(' + this.randomInt(50, 200) + ', ' + this.randomInt(50, 200) + ', ' +
+    this.randomInt(50, 200) + ', ' + this.randomInt(0.5, 1) + ')';
+  }
+
+  randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   generateBarChart(){
@@ -137,87 +184,91 @@ export class DashboardComponent implements OnInit {
     // pie chart:
     this.PieChart = new Chart('pieChart', {
       type: 'pie',
-    data: {
-    labels: this.labels,
-    datasets: [{
-        label: 'Number of Items Sold in Months',
-        data: this.chartData,
-        backgroundColor: this.chartColors,
-        borderColor: this.chartBorders,
-        borderWidth: 1
-    }]
-    },
-    options: {
-    title: {
-        text:'Pie Chart',
-        display: true
-    },
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
+      data: {
+        labels: this.labels,
+        datasets: [{
+          label: "Products Sold Each Month",
+          backgroundColor: this.chartColors,
+          data: this.chartData
         }]
-    }
-    }
-    });
-    this.Charts.push(this.PieChart);
-  }
-
-  getDataList() {
-    this.dataLength = 0;
-    this.dataService.getDataFromFirebase().snapshotChanges().forEach(dataSnapshots => {
-      this.dataList = [];
-      dataSnapshots.forEach(dataSnapshot => {
-        let dataItem = dataSnapshot.payload.val();
-        this.dataAsJSON = dataSnapshot.payload.toJSON();
-        console.log(this.dataAsJSON);
-
-        this.dataLength = Object.keys(this.dataAsJSON).length;
-        console.log(this.dataLength);
-
-        console.log(this.dataAsJSON[7].Month);
-        console.log(this.dataAsJSON[7].Sold);
-        console.log(this.dataAsJSON[15] == null);
-
-        this.updateLocalData();
-
-        dataItem['$key'] = dataSnapshot.key;
-        this.dataKey = dataSnapshot.key;
-        this.dataList.push(dataItem as IData);
-      });
-    });
-  }
-
-  updateLocalData() {
-    let index = 0;
-    while(index < this.dataLength){
-      if(this.dataAsJSON[index] != null) {
-        this.labels.push(this.dataAsJSON[index].Month);
-        this.chartData.push(this.dataAsJSON[index].Sold);
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Pie Chart'
+        }
       }
-      console.log(index);
-      index++;
-    }
-    console.log(this.labels[0]);
-    this.generateColors(this.dataLength);
-    this.generateLineChart();
-    this.generateBarChart();
-    this.generatePieChart();
+    });
   }
 
-  generateColors(numColors: number) {
-    let index = 0;
-    while(index < numColors){
-      let newColor = 'rgba(' + this.randomInt(50, 200) + ', ' + this.randomInt(50, 200) + ', ' +
-      this.randomInt(50, 200) + ', ' + this.randomInt(0.5, 1) + ')';
-      this.chartColors[index] = newColor;
-      this.chartBorders[index] = newColor;
-      index = index + 1;
-    }
+  generateRadarGraph() {
+    this.RadarChart = new Chart('radar-chart', {
+      type: 'radar',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: 'Products Sold Each Month',
+            fill: true,
+            backgroundColor: this.generateColor(),
+            borderColor: this.generateColor(),
+            pointBorderColor: "#fff",
+            pointBackgroundColor: this.generateColor(),
+            data: this.chartData
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Radar Chart'
+        }
+      }
+  });
   }
 
-  randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  generatePolarAreaChart() {
+    this.PolarAreaChart = new Chart('polar-chart', {
+        type: 'polarArea',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Products Sold Each Month",
+            backgroundColor: this.chartColors,
+            data: this.chartData
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Polar Area Chart'
+        }
+      }
+    });
   }
+
+  generateDoughnutChart() {
+    this.DoughnutChart = new Chart('doughnut-chart', {
+      type: 'doughnut',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Products Sold Each Month",
+            backgroundColor: this.chartColors,
+            data: this.chartData
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Doughnut Chart'
+        }
+      }
+    });
+  }
+
 }
