75edc78888bfd8b1179871872181f4f614f764da
Sarthak Bhatnagar <sarthak.bhatnagar@jhuapl.edu>
2019-07-29 14:27:00 -0400

    Removed Fusion Charts as it was causing project to crash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,18 +20,6 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-// Import angular-fusioncharts
-import { FusionChartsModule } from "angular-fusioncharts";
-
-// Import FusionCharts library and chart modules
-import * as FusionCharts from "fusioncharts";
-import * as Charts from "fusioncharts/fusioncharts.charts";
-
-import * as FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
-
-// Pass the fusioncharts library and chart modules
-FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,8 +27,7 @@ FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
     NavComponent,
     DashboardComponent,
     LogoutComponent,
-    FileUploadComponent,
-    FusionChartsModule
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
